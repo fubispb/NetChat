8b26516bab7540b9e0619a6c31dae46ae3b9db3e
fubispb <fubispb@yandex.ru>
2020-01-30 13:09:46 +0300

    Начало разработки.

---
+++ Server/Server.java
@@ -0,0 +1,59 @@
+package NetChat.Server;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+import java.util.Vector;
+
+public class Server {
+    private Vector<ClientHandler> clients;
+
+    public Server() {
+        clients = new Vector<>();
+        ServerSocket server = null;
+        Socket socket = null;
+
+        try {
+            server = new ServerSocket(8189);
+            System.out.println("Сервер запущен!");
+
+            while (true) {
+                socket = server.accept();
+                subscribe(new ClientHandler(this, socket));
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            try {
+                server.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void broadcastMsg(String msg) {
+        for (ClientHandler o: clients) {
+            o.sendMsg(msg);
+        }
+    }
+
+    public void subscribe(ClientHandler client){
+        clients.add(client);
+    }
+
+    public void unsubscribe(ClientHandler client){
+        clients.remove(client);
+    }
+}
---
+++ Server/StartServer.java
@@ -0,0 +1,7 @@
+package NetChat.Server;
+
+public class StartServer {
+    public static void main(String[] args) {
+        new Server();
+    }
+}
